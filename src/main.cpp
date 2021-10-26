33d648e1e33110824ae020deaf0ea3e93f0ce9ba
Daniel Barter <danielbarter@gmail.com>
2021-10-26 14:37:45 -0700

    linting

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,21 @@ int main() {
     TreeSolver tree_solver(42, initial_propensities);
     LinearSolver linear_solver(42, std::move(initial_propensities));
 
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 100000; i++) {
         Event linear_event = linear_solver.event();
         Event tree_event = tree_solver.event();
         if (linear_event.index != tree_event.index ||
             linear_event.dt != tree_event.dt) {
-            std::cout << "non matching event found. index = " << i << '\n';
+            std::cout << "non matching event found." << '\n'
+                      << "step = " << i << '\n';
+
             std::cout << "linear_event: index = "
                       << linear_event.index
-                      << " dt = "
+                      << ", dt = "
                       << linear_event.dt << '\n';
             std::cout << "tree_event: index = "
                       << tree_event.index
-                      << " dt = "
+                      << ", dt = "
                       << tree_event.dt << '\n';
             break;
 
