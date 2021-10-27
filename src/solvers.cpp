14db001c4c580ed6a7a5a31f1ade2748bed393d9
Daniel Barter <danielbarter@gmail.com>
2021-10-27 10:05:53 -0700

    fixing uninitialized vars

--- src/solvers.cpp
+++ src/solvers.cpp
@@ -8,14 +8,16 @@ LinearSolver::LinearSolver(
     unsigned long int seed,
     std::vector<double> initial_propensities) :
     sampler{Sampler(seed)},
-    propensities{initial_propensities} {
+    propensities{initial_propensities},
+    number_of_active_indices{0},
+    propensity_sum{0.0} {
         int i;
         for (i = 0; i < initial_propensities.size(); i++) {
             propensity_sum += initial_propensities[i];
             if (initial_propensities[i] > 0)
                 number_of_active_indices += 1;
         }
-};
+    };
 
 void LinearSolver::update(Update update) {
 
@@ -71,7 +73,8 @@ double LinearSolver::get_propensity_sum() {
 TreeSolver::TreeSolver(
     unsigned long int seed,
     std::vector<double> initial_propensities) :
-    sampler{Sampler(seed)} {
+    sampler{Sampler(seed)},
+    number_of_active_indices{0} {
         int m = 0; // tree depth
         int pow2 = 1; // power of 2 >= numberOfReactions
         number_of_indices = initial_propensities.size();
