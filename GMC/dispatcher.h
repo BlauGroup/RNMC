4b51b8819470d028d7d43713c9609f54862f0a11
Daniel Barter <danielbarter@gmail.com>
2021-11-15 11:10:30 -0800

    factoring out queue structures from GMC

--- GMC/dispatcher.h
+++ GMC/dispatcher.h
@@ -3,87 +3,25 @@
 #include <mutex>
 #include <thread>
 #include "simulation.h"
+#include "../core/queues.h"
 
-
-struct SeedQueue {
-    std::queue<unsigned long int> seeds;
-    std::mutex mutex;
-
-    SeedQueue(unsigned long int number_of_seeds, unsigned long int base_seed) {
-        for (unsigned long int i = base_seed;
-             i < number_of_seeds + base_seed;
-             i++) {
-            seeds.push(i);
-        }
-    }
-
-    std::optional<unsigned long int> get_seed() {
-        std::lock_guard<std::mutex> lock (mutex);
-
-        if (seeds.empty()) {
-            return std::optional<unsigned long int> ();
-        } else {
-            unsigned long int result = seeds.front();
-            seeds.pop();
-            return std::optional<unsigned long int> (result);
-        }
-    }
-};
-
-struct HistoryPacket {
+struct GMCHistoryPacket {
     std::vector<HistoryElement> history;
     unsigned long int seed;
 };
 
-struct HistoryQueue {
-    // the flow of trajectory histories from the simulator threads to
-    // the dispatcher is subtle and important. The vector of histories
-    // is allocated by the simulator. Once the simulation is finished,
-    // it is moved into the history queue. Then the dispatcher moves
-    // it out of the history queue, writes it into the initial state
-    // database and then frees it. Without all the carefully placed
-    // std::move calls, there will be a lot of unnecessary allocations
-    // and frees. In typical C++ fashion, this is made significantly
-    // more complex by the subtle move semantics of std::optional. If
-    // you are going to change this code, you need to use gdb to check
-    // that you haven't accidently introduced extra allocations and
-    // frees (i.e the vector allocated by the simulation thread points
-    // to exactly the same memory as the vector which is used to write
-    // to the initial state database, and every time a move is
-    // supposed to happen, the old reference is actually zerod out).
-    std::queue<HistoryPacket> history_packets;
-    std::mutex mutex;
-
-    void insert_history(HistoryPacket history_packet) {
-        std::lock_guard<std::mutex> lock (mutex);
-        history_packets.push(std::move(history_packet));
-    }
-
-    std::optional<HistoryPacket> get_history() {
-        std::lock_guard<std::mutex> lock (mutex);
-        if (history_packets.empty()) {
-            return std::optional<HistoryPacket> ();
-        } else {
-            HistoryPacket result = std::move(history_packets.front());
-            history_packets.pop();
-            return std::optional<HistoryPacket> (std::move(result));
-
-        }
-    };
-
-};
 
 
 template <typename Solver>
 struct SimulatorPayload {
     ReactionNetwork &reaction_network;
-    HistoryQueue &history_queue;
+    HistoryQueue<GMCHistoryPacket> &history_queue;
     SeedQueue &seed_queue;
     int step_cutoff;
 
     SimulatorPayload(
         ReactionNetwork &reaction_network,
-        HistoryQueue &history_queue,
+        HistoryQueue<GMCHistoryPacket> &history_queue,
         SeedQueue &seed_queue,
         int step_cutoff
         ) :
@@ -107,7 +45,7 @@ struct SimulatorPayload {
             simulation.history.resize(simulation.step);
             history_queue.insert_history(
                 std::move(
-                    HistoryPacket {
+                    GMCHistoryPacket {
                         .history = std::move(simulation.history),
                         .seed = seed
                         }));
@@ -122,7 +60,7 @@ struct Dispatcher {
     ReactionNetwork reaction_network;
     SqlStatement<TrajectoriesSql> trajectories_stmt;
     SqlWriter<TrajectoriesSql> trajectories_writer;
-    HistoryQueue history_queue;
+    HistoryQueue<GMCHistoryPacket> history_queue;
     SeedQueue seed_queue;
     std::vector<std::thread> threads;
     int step_cutoff;
@@ -160,7 +98,7 @@ struct Dispatcher {
         {};
 
     void run_dispatcher();
-    void record_simulation_history(HistoryPacket history_packet);
+    void record_simulation_history(GMCHistoryPacket history_packet);
 };
 
 
@@ -184,11 +122,11 @@ void Dispatcher<Solver>::run_dispatcher() {
     int trajectories_written = 0;
     while (trajectories_written < number_of_simulations) {
 
-        std::optional<HistoryPacket>
+        std::optional<GMCHistoryPacket>
             maybe_history_packet = history_queue.get_history();
 
         if (maybe_history_packet) {
-            HistoryPacket history_packet = std::move(maybe_history_packet.value());
+            GMCHistoryPacket history_packet = std::move(maybe_history_packet.value());
             record_simulation_history(std::move(history_packet));
             trajectories_written += 1;
         };
@@ -207,7 +145,7 @@ void Dispatcher<Solver>::run_dispatcher() {
 };
 
 template <typename Solver>
-void Dispatcher<Solver>::record_simulation_history(HistoryPacket history_packet) {
+void Dispatcher<Solver>::record_simulation_history(GMCHistoryPacket history_packet) {
     int count = 0;
     constexpr int transaction_size = 20000;
     initial_state_database.exec("BEGIN");
---
+++ core/queues.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <queue>
+#include <mutex>
+#include <optional>
+
+
+
+
+struct SeedQueue {
+    std::queue<unsigned long int> seeds;
+    std::mutex mutex;
+
+    SeedQueue(unsigned long int number_of_seeds, unsigned long int base_seed) {
+        for (unsigned long int i = base_seed;
+             i < number_of_seeds + base_seed;
+             i++) {
+            seeds.push(i);
+        }
+    }
+
+    std::optional<unsigned long int> get_seed() {
+        std::lock_guard<std::mutex> lock (mutex);
+
+        if (seeds.empty()) {
+            return std::optional<unsigned long int> ();
+        } else {
+            unsigned long int result = seeds.front();
+            seeds.pop();
+            return std::optional<unsigned long int> (result);
+        }
+    }
+};
+
+
+template <typename T>
+struct HistoryQueue {
+    // the flow of trajectory histories from the simulator threads to
+    // the dispatcher is subtle and important. The vector of histories
+    // is allocated by the simulator. Once the simulation is finished,
+    // it is moved into the history queue. Then the dispatcher moves
+    // it out of the history queue, writes it into the initial state
+    // database and then frees it. Without all the carefully placed
+    // std::move calls, there will be a lot of unnecessary allocations
+    // and frees. In typical C++ fashion, this is made significantly
+    // more complex by the subtle move semantics of std::optional. If
+    // you are going to change this code, you need to use gdb to check
+    // that you haven't accidently introduced extra allocations and
+    // frees (i.e the vector allocated by the simulation thread points
+    // to exactly the same memory as the vector which is used to write
+    // to the initial state database, and every time a move is
+    // supposed to happen, the old reference is actually zerod out).
+    std::queue<T> history_packets;
+    std::mutex mutex;
+
+    void insert_history(T history_packet) {
+        std::lock_guard<std::mutex> lock (mutex);
+        history_packets.push(std::move(history_packet));
+    }
+
+    std::optional<T> get_history() {
+        std::lock_guard<std::mutex> lock (mutex);
+        if (history_packets.empty()) {
+            return std::optional<T> ();
+        } else {
+            T result = std::move(history_packets.front());
+            history_packets.pop();
+            return std::optional<T> (std::move(result));
+
+        }
+    };
+
+};
+
