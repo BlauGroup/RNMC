faeffa5198a93e46635d462967add75c75d11a79
Daniel Barter <danielbarter@gmail.com>
2021-11-23 11:06:54 -0800

    2 threads for tests

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ GMC_TEST_DIR="./test_materials/GMC"
 
 cp $GMC_TEST_DIR/initial_state.sqlite $GMC_TEST_DIR/initial_state_copy.sqlite
 
-./build/GMC --reaction_database=$GMC_TEST_DIR/rn.sqlite --initial_state_database=$GMC_TEST_DIR/initial_state_copy.sqlite --number_of_simulations=1000 --base_seed=1000 --thread_count=8 --step_cutoff=200 --dependency_threshold=1
+./build/GMC --reaction_database=$GMC_TEST_DIR/rn.sqlite --initial_state_database=$GMC_TEST_DIR/initial_state_copy.sqlite --number_of_simulations=1000 --base_seed=1000 --thread_count=2 --step_cutoff=200 --dependency_threshold=1
 
 sql='SELECT seed, step, reaction_id FROM trajectories ORDER BY seed ASC, step ASC;'
 
