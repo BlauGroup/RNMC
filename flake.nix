b3674af2c84db0807af7550b32315a0b45587e8a
Daniel Barter <danielbarter@gmail.com>
2021-11-23 11:05:24 -0800

    CI

--- flake.nix
+++ flake.nix
@@ -3,66 +3,73 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
 
-    devShell.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      mkShell {
+    # RNMC is so simple that the build derivation looks exactly the same on
+    # all platforms.
+    let genericDefaultPackage = systemString:
+          with import nixpkgs { system = systemString; };
+          stdenv.mkDerivation {
+            name = "RNMC";
+            src = self;
 
-        buildInputs = [
-          gcc
-          clang
-          gsl
-          sqlite
-          sqlitebrowser
-          gdb
-          valgrind
-        ];
-
-        # environment for CLANGD
-        # CPATH=$CLANGD_PATH emacs
-        CLANGD_PATH = builtins.concatStringsSep ":" [
-
-          # C++ stdlib headers
-          "${gcc-unwrapped}/include/c++/10.3.0"
-          "${gcc-unwrapped}/include/c++/10.3.0/x86_64-unknown-linux-gnu"
-
-          # libc headers
-          "${glibc.dev}/include"
-
-          # compiler specific headers
-          "${clang}/resource-root/include"
-
-          "${sqlite.dev}/include"
-          "${gsl}/include"
-        ];
-      };
-
-    defaultPackage =
-      # RNMC is so simple that the build derivation looks exactly the same on
-      # all platforms.
-      let genericDefaultPackage = systemString:
-            with import nixpkgs { system = systemString; };
-            stdenv.mkDerivation {
-              name = "RNMC";
-              src = self;
-
-              buildInputs = [
-                clang
-                gsl
-                sqlite
-              ];
+            buildInputs = [
+              clang
+              gsl
+              sqlite
+            ];
 
 
-              buildPhase = "CC=clang++ ./build.sh";
-              installPhase = "mkdir -p $out/bin; mv ./build/* $out/bin";
+            buildPhase = "CC=clang++ ./build.sh";
+            installPhase = "mkdir -p $out/bin; mv ./build/* $out/bin";
+            doCheck = true;
+            checkPhase = "./test.sh";
 
-            };
-
-
+          };
       in {
-        x86_64-linux = genericDefaultPackage "x86_64-linux";
-        x86_64-darwin = genericDefaultPackage "x86_64-darwin";
+        devShell.x86_64-linux =
+          with import nixpkgs { system = "x86_64-linux"; };
+          mkShell {
+
+            buildInputs = [
+              gcc
+              clang
+              gsl
+              sqlite
+              sqlitebrowser
+              gdb
+              valgrind
+            ];
+
+            # environment for CLANGD
+            # CPATH=$CLANGD_PATH emacs
+            CLANGD_PATH = builtins.concatStringsSep ":" [
+
+              # C++ stdlib headers
+              "${gcc-unwrapped}/include/c++/10.3.0"
+              "${gcc-unwrapped}/include/c++/10.3.0/x86_64-unknown-linux-gnu"
+
+              # libc headers
+              "${glibc.dev}/include"
+
+              # compiler specific headers
+              "${clang}/resource-root/include"
+
+              "${sqlite.dev}/include"
+              "${gsl}/include"
+            ];
+          };
+
+        defaultPackage = {
+          x86_64-linux = genericDefaultPackage "x86_64-linux";
+          x86_64-darwin = genericDefaultPackage "x86_64-darwin";
+        };
+
+      checks = {
+        x86_64-linux.tests = genericDefaultPackage "x86_64-linux";
+        x86_64-darwin.tests = genericDefaultPackage "x86_64-darwin";
+
       };
-  };
+
+      };
 }
