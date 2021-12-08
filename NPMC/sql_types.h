a514c339e94ca608bb634ab97cf1eb1543a458f1
Daniel Barter <danielbarter@gmail.com>
2021-12-07 16:48:19 -0800

    writing NPMC sql types

--- NPMC/sql_types.h
+++ NPMC/sql_types.h
@@ -9,6 +9,14 @@ struct SpecieSql {
     static void action(SpecieSql &r, sqlite3_stmt *stmt);
 };
 
+std::string SpecieSql::sql_statement =
+    "SELECT species_id, degrees_of_freedom FROM species;";
+
+void SpecieSql::action(SpecieSql &r, sqlite3_stmt *stmt) {
+    r.species_id = sqlite3_column_int(stmt, 0);
+    r.degrees_of_freedom = sqlite3_column_int(stmt, 1);
+};
+
 
 struct SiteSql {
     int site_id;
@@ -19,6 +27,18 @@ struct SiteSql {
     static std::string sql_statement;
     static void action(SiteSql &r, sqlite3_stmt *stmt);
 };
+
+std::string SiteSql::sql_statement =
+    "SELECT site_id, x, y, z, species_id FROM sites;";
+
+
+void SiteSql::action(SiteSql &r, sqlite3_stmt *stmt) {
+    r.site_id = sqlite3_column_int(stmt, 0);
+    r.x = sqlite3_column_double(stmt, 1);
+    r.y = sqlite3_column_double(stmt, 2);
+    r.z = sqlite3_column_double(stmt, 3);
+    r.species_id = sqlite3_column_int(stmt, 4);
+}
 
 
 struct InteractionSql {
@@ -31,7 +51,40 @@ struct InteractionSql {
     int right_state_2;
     double rate;
     static std::string sql_statement;
-    static void action(SiteSql &r, sqlite3_stmt *stmt);
+    static void action(InteractionSql &r, sqlite3_stmt *stmt);
 };
 
+std::string InteractionSql::sql_statement =
+    "SELECT interaction_id, species_id_1, species_id_2, "
+    "left_state_1, left_state_2, right_state_1, right_state_2, "
+    "rate FROM intearctions;";
 
+void InteractionSql::action(InteractionSql &r, sqlite3_stmt *stmt) {
+    r.interaction_id = sqlite3_column_int(stmt, 0);
+    r.species_id_1 = sqlite3_column_int(stmt, 1);
+    r.species_id_2 = sqlite3_column_int(stmt, 2);
+    r.left_state_1 = sqlite3_column_int(stmt, 3);
+    r.left_state_2 = sqlite3_column_int(stmt, 4);
+    r.right_state_1 = sqlite3_column_int(stmt, 5);
+    r.right_state_2 = sqlite3_column_int(stmt, 6);
+    r.rate = sqlite3_column_double(stmt, 7);
+}
+
+
+struct MetadataSql {
+    int number_of_species;
+    int number_of_sites;
+    int number_of_interactions;
+    static std::string sql_statement;
+    static void action(MetadataSql &r, sqlite3_stmt *stmt);
+};
+
+std::string MetadataSql::sql_statement =
+    "SELECT number_of_species, number_of_sites, "
+    "number_of_interactions FROM metadata;";
+
+void MetadataSql::action(MetadataSql &r, sqlite3_stmt *stmt) {
+    r.number_of_species = sqlite3_column_int(stmt, 0);
+    r.number_of_sites = sqlite3_column_int(stmt, 1);
+    r.number_of_interactions = sqlite3_column_int(stmt, 2);
+};
