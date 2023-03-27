177ade7bcbf0751c5d01926837db74152f89f241
Laura Zichi <laurazichi@Lauras-MacBook-Pro-3.local>
2023-03-27 17:12:23 -0400

    test files for GMC and NPMC updated

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ mkdir -p build
 flags="-fno-rtti -fno-exceptions -std=c++17 -Wall -Wextra -g $(gsl-config --cflags) $(gsl-config --libs) -lsqlite3 -lpthread"
 
 echo "building test_core"
-$CC $flags ./core/test.cpp -o ./build/test_core
+$CXX $flags ./core/test.cpp -o ./build/test_core
+echo "building GMC"
+$CXX $flags ./GMC/GMC.cpp -o ./build/GMC
 echo "building NPMC"
-$CC $flags ./NPMC/NPMC.cpp -o ./build/NPMC
+$CXX $flags ./NPMC/NPMC.cpp -o ./build/NPMC
