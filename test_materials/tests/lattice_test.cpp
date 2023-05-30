20ab25d82d2f490e9d9966b8d5fd960b5928d0cc
Laura Zichi <laurazichi@Lauras-MacBook-Pro-3.local>
2023-05-30 18:11:46 -0400

    updated lattice test for gtest framework

--- test_materials/tests/lattice_test.cpp
+++ test_materials/tests/lattice_test.cpp
@@ -9,20 +9,21 @@
 #include "../../LGMC/lattice.h"
 
 // test that lattice has correct dimensions
-TEST(LatticeIsConstructed, Lattice) {
+TEST(create_basic_lattice, Lattice) {
   
    // create pointer to simple lattice
    Lattice *lattice = new Lattice(1, 3, 5, 7);
    ASSERT_EQ(lattice->xlo, 0);
-   /*REQUIRE(lattice->xhi == 3);
-   REQUIRE(lattice->ylo == 0);
-   REQUIRE(lattice->yhi == 5);
-   REQUIRE(lattice->zlo == 0);
-   REQUIRE(lattice->zhi == 7);
-   REQUIRE(lattice->latconst == 1);
+   ASSERT_EQ(lattice->xhi, 3);
+   ASSERT_EQ(lattice->ylo, 0);
+   ASSERT_EQ(lattice->yhi, 5);
+   ASSERT_EQ(lattice->zlo, 0);
+   ASSERT_EQ(lattice->zhi, 7);
+   ASSERT_EQ(lattice->latconst, 1);
+   
    // x, y periodic and z non-periodic
-   REQUIRE(lattice->sites.size() == 3*5*(7 + 1));
-   REQUIRE(lattice->edges.size() == 3*5);*/
+   ASSERT_EQ(static_cast<int>(lattice->sites.size()), 3*5*(7 + 1));
+   ASSERT_EQ(static_cast<int>(lattice->edges.size()), 3*5);
 
 
 }
@@ -31,50 +32,42 @@ TEST(LatticeIsConstructed, Lattice) {
 // test that each site has the correct neighbors
 
 
-
-/*
 // test add site
-TEST_CASE("Site added to existing lattice", "[add_site]") {
+TEST(add_site, add_site) {
    Lattice *lattice = new Lattice(1, 3, 5, 7);
-
 
    // z dimension grows
    lattice->add_site(2, 4, 8, 2, 4, 8, true, true, true);
-
 
    std::tuple<uint32_t, uint32_t, uint32_t> key = {2, 4, 8};
    int site_added = lattice->loc_map[key];
 
 
-   REQUIRE(lattice->zhi == 8);
-   REQUIRE(lattice->edges.size() == 3*5 + 1);
-   REQUIRE(lattice->sites.size() == 3*5*(7+1) + 1);
+   ASSERT_EQ(lattice->zhi, 8);
+   ASSERT_EQ(static_cast<int>(lattice->edges.size()), 3*5 + 1);
+   ASSERT_EQ(static_cast<int>(lattice->sites.size()), 3*5*(7+1) + 1);
   
    key = {2, 4, 7};
    int id = lattice->loc_map[key];
   
-   REQUIRE(int(lattice->numneigh[site_added]) == 1);
-   REQUIRE(int(lattice->idneigh[site_added][0]) == id);
+   ASSERT_EQ(int(lattice->numneigh[site_added]), 1);
+   ASSERT_EQ(int(lattice->idneigh[site_added][0]), id);
 }
 
 
 // test delete site
-TEST_CASE("Site deleted from existing lattice", "[delete_site]") {
+TEST(delete_site, delete_site) {
    Lattice *lattice = new Lattice(1, 3, 5, 7);
-
 
    std::tuple<uint32_t, uint32_t, uint32_t> key = {2, 4, 7};
    int id = lattice->loc_map[key];
 
-
    lattice->delete_site(id);
 
-
-   REQUIRE(lattice->zhi == 7);
-   REQUIRE(lattice->edges.size() == 3*5 - 1);
-   REQUIRE(lattice->sites.size() == 3*5*(7+1) - 1);
+   ASSERT_EQ(lattice->zhi, 7);
+   ASSERT_EQ(static_cast<int>(lattice->edges.size()), 3*5 - 1);
+   ASSERT_EQ(static_cast<int>(lattice->sites.size()), 3*5*(7+1) - 1);
 }
 
 
 // test copy constructor
-*/
